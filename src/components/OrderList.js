77e2be5a5b58ac229c6339bfce2f0fb4967f0619
aryyo <hobra001@ucr.edu>
2025-01-04 10:06:53 -0800

    Added bento box for orderList

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,67 +1,68 @@
-import '../styles/OrderList.css';
-import OrderData from '../data/OrderData';
+import "../styles/OrderList.css";
+import OrderData from "../data/OrderData";
 
 const formatName = (name) => {
-    if (name.length > 18) {
-      const clippedName = name.slice(0, 15);
-      return `${clippedName}...`;
-    } else {
-      return name;
-    }
-  };
-  
-  const formatDate = (date) => {
-    const month = date.slice(5, 7);
-    const day = date.slice(8, 10);
-    switch (month) {
-      case "01":
-        return `Jan ${day}`;
-      case "02":
-        return `Feb ${day}`;
-      case "03":
-        return `Mar ${day}`;
-      case "04":
-        return `Apr ${day}`;
-      case "05":
-        return `May ${day}`;
-      case "06":
-        return `Jun ${day}`;
-      case "07":
-        return `Jul ${day}`;
-      case "08":
-        return `Aug ${day}`;
-      case "09":
-        return `Sep ${day}`;
-      case "10":
-        return `Oct ${day}`;
-      case "11":
-        return `Nov ${day}`;
-      case "12":
-        return `Dec ${day}`;
-    }
-  };
-  
-  const formatTotal = (total) => {
-    return `$${total}`;
-  };
-  
-  const formatOrderId = (id) => {
-    return `#${id}`;
-  };
-  
-  const columns = [
-    { label: "Order", key: "id", formatter: formatOrderId },
-    { label: "Customer", key: "customerName", formatter: formatName },
-    { label: "Type", key: "type" },
-    { label: "Status", key: "status" },
-    { label: "Product", key: "product" },
-    { label: "Total", key: "total", formatter: formatTotal },
-    { label: "Date", key: "datePlaced", formatter: formatDate },
-  ];
+  if (name.length > 18) {
+    const clippedName = name.slice(0, 15);
+    return `${clippedName}...`;
+  } else {
+    return name;
+  }
+};
+
+const formatDate = (date) => {
+  const month = date.slice(5, 7);
+  const day = date.slice(8, 10);
+  switch (month) {
+    case "01":
+      return `Jan ${day}`;
+    case "02":
+      return `Feb ${day}`;
+    case "03":
+      return `Mar ${day}`;
+    case "04":
+      return `Apr ${day}`;
+    case "05":
+      return `May ${day}`;
+    case "06":
+      return `Jun ${day}`;
+    case "07":
+      return `Jul ${day}`;
+    case "08":
+      return `Aug ${day}`;
+    case "09":
+      return `Sep ${day}`;
+    case "10":
+      return `Oct ${day}`;
+    case "11":
+      return `Nov ${day}`;
+    case "12":
+      return `Dec ${day}`;
+  }
+};
+
+const formatTotal = (total) => {
+  return `$${total}`;
+};
+
+const formatOrderId = (id) => {
+  return `#${id}`;
+};
+
+const columns = [
+  { label: "Order", key: "id", formatter: formatOrderId },
+  { label: "Customer", key: "customerName", formatter: formatName },
+  { label: "Type", key: "type" },
+  { label: "Status", key: "status" },
+  { label: "Product", key: "product" },
+  { label: "Total", key: "total", formatter: formatTotal },
+  { label: "Date", key: "datePlaced", formatter: formatDate },
+];
 
 const OrderList = () => {
-    return (
-        <div className="orders-table">
+  return (
+    <div className="orders-table-wrapper">
+      <div className="orders-table">
         <div className="table-header">
           {columns.map((column) => {
             return <p key={column.key}>{column.label}</p>;
@@ -75,8 +76,18 @@ const OrderList = () => {
                 const formatter = column.formatter;
                 return (
                   <div className="table-column" key={column.key}>
-                    {order.status === value ? <img src={`../icons/${value}.png`}  alt=""/> : ''}
-                    <p className={order.status === value ? order.status.toLowerCase() : ''}>{formatter ? formatter(value) : value}</p>
+                    {order.status === value ? (
+                      <img src={`../icons/${value}.png`} alt="" />
+                    ) : (
+                      ""
+                    )}
+                    <p
+                      className={
+                        order.status === value ? order.status.toLowerCase() : ""
+                      }
+                    >
+                      {formatter ? formatter(value) : value}
+                    </p>
                   </div>
                 );
               })}
@@ -84,7 +95,8 @@ const OrderList = () => {
           );
         })}
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default OrderList;
