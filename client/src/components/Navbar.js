1164ec61f696d967fe30c7a6b56261a4707f6bba
aryyo <hobra001@ucr.edu>
2025-04-04 04:05:05 -0700

    Updated theme slider

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -196,7 +196,20 @@ const Navbar = ({ onToggleSidebar }) => {
               </div>
               <div className="dropdown-divider" />
               <button className="dropdown-item" onClick={handleEditClick}>
-                <span>{Icons.edit}</span>
+                <span>
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3Z" />
+                  </svg>
+                </span>
                 Edit Profile
               </button>
               <div className="dropdown-divider" />
