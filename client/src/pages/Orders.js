b7c2e9c14800a8f4363798b270b1cae24b8dad7b
aryyo <hobra001@ucr.edu>
2025-04-09 04:06:50 -0700

    Updated remove button

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -135,7 +135,7 @@ const Orders = () => {
             <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
             <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
           </svg>
-          {isSelectionMode 
+          {isSelectionMode && selectedOrders.size > 0 
             ? `Remove (${selectedOrders.size})`
             : 'Remove'
           }
